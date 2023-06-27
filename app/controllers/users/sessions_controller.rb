2dfec9c5b7d65cd7b534ae761cbfff2b1aef987a
Sourav Mandal <sourav.mandal@wonderbotz.com>
2023-06-27 13:38:15 +0530

    Authentication and authorization working

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  include RackSessionsFix
+  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
+  private
+  def respond_with(current_user, _opts = {})
+    render json: {
+      status: { 
+        code: 200, message: 'Logged in successfully.',
+        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
+      }
+    }, status: :ok
+  end
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, "95e5db973350c7c7d645613e56606672862f5753157ea9cb65038800845e67c2eeffffad7bd3cf7c63e371283b9f928fe89a9476012c36ecf8f11bc77c6938e8").first
+      current_user = User.find(jwt_payload['sub'])
+    end
+    
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
+  end
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
